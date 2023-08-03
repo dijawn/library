5a46d8e249acf98fa439d596e4d17a2163daabea
John Denos <john.denosiii@gmail.com>
2023-08-03 15:09:23 -0400

    Refactor generic object constructors to classes with methods. Update UI to accommodate screen size

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const close = document.querySelectorAll(".close");
 const submitBtn = document.querySelector(".submit");
 let greenhouse = document.querySelector(".greenhouse");
 const background = document.querySelector(".bg");
+const nameInput = document.querySelector("#plantName");
+const validationText = document.querySelectorAll(".validation-text");
+const allInputs = document.querySelectorAll("input");
 
 btn.addEventListener('click', e =>{
     modal.style.display = "grid";
@@ -20,21 +23,14 @@ for(i=0; i<close.length; i++){
 
 let myGreenhouse = [];
 
-function Plant(plantName, latinName, humidity, light){
-    this.plantName = plantName
-    this.latinName = latinName
-    this.humidity = humidity
-    this.light = light
-};
 
-
-function addPlantToGreenhouse(){
-    let plantName = document.querySelector("#plantName").value;
-    let latinName = document.querySelector("#latinName").value;
-    let humidity = document.querySelector("#humidity").value;
-    let light = document.querySelector("#light").value;
-    let newPlant = new Plant(plantName, latinName, humidity, light);
-    myGreenhouse.push(newPlant);
+class Plant {
+    constructor(plantName, latinName, humidity, light){
+        this.plantName = document.querySelector("#plantName").value;
+        this.latinName = document.querySelector("#latinName").value;
+        this.humidity = document.querySelector("#humidity").value;
+        this.light = document.querySelector("#light").value;
+    }
 }
 
 function displayPlant() {
@@ -52,11 +48,11 @@ function displayPlant() {
 
         let humidityDisplay = document.createElement('span');
         humidityDisplay.classList.add('humidityDisplay')
-        humidityDisplay.textContent = `Humidity Needs: ${myGreenhouse[i].humidity}`;
+        humidityDisplay.textContent = `Humidity: ${myGreenhouse[i].humidity}`;
 
         let lightReqs = document.createElement('span');
         lightReqs.classList.add("lightReqs");
-        lightReqs.textContent = `Light Needs: ${myGreenhouse[i].light}`;
+        lightReqs.textContent = `Lighting: ${myGreenhouse[i].light}`;
 
         let removeBtn = document.createElement('button');
         removeBtn.classList.add("remove-style");
@@ -78,12 +74,16 @@ function removePlant(index){
 };
 
 
+
 submitBtn.addEventListener('click', e => {
-    addPlantToGreenhouse();
+    let newPlant = new Plant(plantName, latinName, humidity, light);
+    myGreenhouse.push(newPlant);
+    
     displayPlant();
     modal.style.display = "none";
     background.style.filter = "blur(0)";
 });
+
 
 window.addEventListener('keypress', e => {
     if ((modal.style.display === 'grid' && e.key === "Enter")){
